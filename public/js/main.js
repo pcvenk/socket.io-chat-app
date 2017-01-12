fc1d1a6bc3f26e7b9c926e924cad5e5577ae662f
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-01-12 18:42:32 +0100

    Done: User display on the right

--- public/js/main.js
+++ public/js/main.js
@@ -24,3 +24,12 @@ userForm.submit(function(e){
         }
     })
 });
+
+//Display Users
+socket.on('users', function(data){
+    var text = '';
+    for(var i = 0; i < data.length; i++){
+        text += '<li class="list-group-item userlist">'+data[i]+'</li>'
+    }
+    users.html(text);
+});
