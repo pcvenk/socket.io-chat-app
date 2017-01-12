d4b97fa7620f6db329d33c028f1c51dd9b7bfd6f
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-01-12 15:49:21 +0100

    Established connection with the socket on the back-end, set up the user name and update users function

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.engine('jade', require('jade').__express);
 //Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Connect to Socket
+io.socket.on('connection', function(socket){
+   //Set UserName
+   socket.on('set user', function(data, cb){
+      if(users.indexOf(data) != -1){
+         cb(false)
+      } else {
+         cb(true);
+         socket.username = data;
+         users.push(socket.username);
+         updateUsers();
+      }
+   });
+
+   function updateUsers(){
+      io.socket.emit('users', users);
+   }
+});
+
 //Index Route
 app.get('/', function(req, res){
    res.render('index');
